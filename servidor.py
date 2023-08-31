9791b46e8de6613e07aadca3b5e16f3a26f7887b
A01752113 <a01752113@tec.mx>
2023-08-31 17:33:49 -0600

    actualizacion 3

--- servidor.py
+++ servidor.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import numpy as np
+from joblib import load
+import os
 
+#cargar el modelo
+dt=load('dt1.joblib')
 
 #generar el servidor(Back-end)
 servidorWeb = Flask(__name__)
@@ -8,5 +13,14 @@ servidorWeb = Flask(__name__)
 def holamundo():
         return render_template('pagina1.html')
 
+#envio de datos a traves de JSON
+@servidorWeb.route('/modelo',methods=['POST'])
+def modeloPrediccion():
+        contenido=request.json
+        print(contenido)
+        return jsonify({'resultado':"Hola"})
+
+
 if __name__ == '__main__':
         servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
+

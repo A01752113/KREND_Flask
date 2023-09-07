69d227572ea38eb998711d1a73f07e2b6180708c
A01752113 <a01752113@tec.mx>
2023-09-06 19:37:45 -0600

    update form

--- servidor.py
+++ servidor.py
@@ -29,6 +29,26 @@ def modeloPrediccion():
         resultado=dt.predict(datosEntrada.reshape(1,-1))
         return jsonify({'resultado': str(resultado[0])})
 
+@servidorWeb.route("/holamundo",methods=['GET'])
+def holamundo():
+        return render_template('pagina1.html')
+
+#envio de datos a traves de JSON
+@servidorWeb.route('/modeloForm',methods=['POST'])
+def modeloForm():
+        contenido=request.form
+        print(contenido)
+        datosEntrada=np.array([
+                0.88,0,2.6,0.098,25,67,0.9968,1,0.4,
+                contenido['pH'],
+                contenido['sulphates'],
+                contenido['alcohol']
+
+        ])
+        #utilizar el modelo
+        resultado=dt.predict(datosEntrada.reshape(1,-1))
+        return jsonify({'resultado': str(resultado[0])})
+
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
